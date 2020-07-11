f165e9c9d2b03e277c5d8cf3daa491dc570509f2
mlituev <mlituev@kodeks.ru>
2020-07-11 12:17:39 +0300

    [client] styled with breakpoints implemented.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,17 @@
-import { Provider } from 'react-redux'
 import App from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux'
 import { withReduxStore } from '../src/utils'
+import { theme } from '../src/theme'
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, store } = this.props
     return (
       <Provider store={store}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
     )
   }
--- client/pages/index.js
+++ client/pages/index.js
@@ -5,17 +5,27 @@ import {
   resetCount,
 } from '../src/store/actions/counter.action'
 
+import styled from 'styled-components'
+import { down } from 'styled-breakpoints'
+
 export default () => {
   const count = useSelector(({ counter }) => counter)
   const dispatch = useDispatch()
 
   return (
-    <div>
+    <Wrapper>
       <h1>Counter: {count}</h1>
 
       <button onClick={() => dispatch(incrementCount())}>+1</button>
       <button onClick={() => dispatch(decrementCount())}>-1</button>
       <button onClick={() => dispatch(resetCount())}>Reset</button>
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.color.success};
+  ${down('sm')} {
+    background-color: ${({ theme }) => theme.color.light};
+  }
+`
